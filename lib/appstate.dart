c38cc8110590704446211a2fca6bb1525ba3cf62
mehernigar17 <mehernigar1744@gmail.com>
2026-02-10 22:31:18 +0600

    working appstate

--- lib/appstate.dart
+++ lib/appstate.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+class Appstate  extends ChangeNotifier{
+  bool _isDark=false;
+  bool _isBangla=false;
+  bool  get isDark=> _isDark;
+  bool get isBangla=>_isBangla;
+  void setDark(bool value){
+    _isDark=value;
+    notifyListeners();
+
+  }
+  void setLanguage(bool value){
+    _isBangla=value;
+    notifyListeners();
+  }
+
+}
