75a6da9865c8f868bc110f11aab8cc7a8278a686
mehernigar17 <mehernigar1744@gmail.com>
2026-02-21 14:05:19 +0600

    necessary information included in both ssc and hsc

---
+++ lib/hscFormFillup.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class hscFormFillPage extends StatefulWidget {
+  const hscFormFillPage({super.key});
+
+  @override
+  State<hscFormFillPage> createState() => _hscFormFillPageState();
+}
+
+class _hscFormFillPageState extends State<hscFormFillPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/sscFormFillup.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class sscFormFillPage extends StatefulWidget {
+  const sscFormFillPage({super.key});
+
+  @override
+  State<sscFormFillPage> createState() => _sscFormFillPageState();
+}
+
+class _sscFormFillPageState extends State<sscFormFillPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
